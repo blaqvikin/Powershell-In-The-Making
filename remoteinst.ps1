994a3119d89b70356c2cdc0a65b16882acbcb243
blaqvikin <59708914+blaqvikin@users.noreply.github.com>
2020-01-21 12:44:21 +0200

    Update remoteinst.ps1

--- remoteinst.ps1
+++ remoteinst.ps1
@@ -58,7 +58,7 @@ Try {
  
 ########## Define the windows path to the downloaded/ downloads file/ folder. Next download and place the temp file to the desired folder below.
 
-wget http://102.37.14.239/1510WindowsAgentSetup.exe -O $DownloadsFolder\1510WindowsAgentSetup.exe
+wget https://github.com/blaqvikin/customdownlods/blob/execrepo/1531WindowsAgentSetup.exe -O $DownloadsFolder\1510WindowsAgentSetup.exe
 
 $DownloadsFolder=Get-ItemPropertyValue 'HKCU:\software\microsoft\windows\currentversion\explorer\shell folders\' -Name '{374DE290-123F-4565-9164-39C4925E467B}'
 
