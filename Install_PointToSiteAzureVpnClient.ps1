d6a8d82a73fd5c8b3937524eaef25f6ff70d8eba
blaqvikin <mawanda.mlalandle@outlook.com>
2021-10-11 20:43:32 +0200

    Update variables on script, removed sensitive values

--- Install_PointToSiteAzureVpnClient.ps1
+++ Install_PointToSiteAzureVpnClient.ps1
@@ -3,14 +3,14 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser #Enable exe
     #Declare the downloads folder, this is default reg key for all Windows machines
 $DownloadsFolder=Get-ItemPropertyValue 'HKCU:\software\microsoft\windows\currentversion\explorer\shell folders\' -Name '{374DE290-123F-4565-9164-39C4925E467B}'
 
-   Invoke-WebRequest -uri "https://demowvdsto.blob.core.windows.net/public/Fund-Prod-VNet-Gw.zip?sv=2020-04-08&si=RL-EOL-DEC&sr=b&sig=ODuFgTJhjI1vfvC1USY%2BZWBpJ%2FHf3DYVVR1szt2lBoQ%3D" `
-   -OutFile $DownloadsFolder\Fund-Prod-VNet-Gw.zip
+   Invoke-WebRequest -uri "https://mydownloadsite.com" `
+   -OutFile $DownloadsFolder\ArchiveFolder.zip
     
-   Expand-Archive $DownloadsFolder\Fund-Prod-VNet-Gw.zip #Extract the zip file
+   Expand-Archive $DownloadsFolder\ArchiveFolder.zip #Extract the zip file
 
-$secureString = convertto-securestring "Fund1_K3y$" -AsPlainText -force
+$secureString = convertto-securestring "CertificatePassword" -AsPlainText -force
 
-   Import-PfxCertificate -FilePath $DownloadsFolder\Fund-Prod-VNet-Gw\Fund-Prod-VNet-Gw\Generic\Fundchildcert.pfx `
+   Import-PfxCertificate -FilePath $DownloadsFolder\ArchiveFolder\Generic\ChildCert.pfx `
    -CertStoreLocation Cert:\CurrentUser\My -Password $secureString #Import the child cert with password
 
-   Invoke-Command -ScriptBlock {Start-Process $DownloadsFolder\Fund-Prod-VNet-Gw\Fund-Prod-VNet-Gw\WindowsAmd6\VpnClientSetupAmd64.exe} #Install the VPN client software.
+   Invoke-Command -ScriptBlock {Start-Process $DownloadsFolder\ArchiveFolder\Generic\WindowsAmd6\VpnClientSetupAmd64.exe} #Install the VPN client software.
