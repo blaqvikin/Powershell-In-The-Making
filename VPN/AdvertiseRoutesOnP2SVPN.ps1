345a4e92b3b69165a641634e709df559746264f6
blaqvikin <mawanda.hlophoyi@gmail.com>
2022-03-30 11:50:48 +0200

    update comments

--- VPN/AdvertiseRoutesOnP2SVPN.ps1
+++ VPN/AdvertiseRoutesOnP2SVPN.ps1
@@ -1,2 +1,4 @@
 $gw = Get-AzVirtualNetworkGateway -Name "<NameOfVirtualNetworkGateway>" -ResourceGroupName "<ResourceGroupName>"
 Set-AzVirtualNetworkGateway -VirtualNetworkGateway $gw -CustomRoute <routeCIDRaddress>
+
+#check route print to validate.
