ce35bc57112e17ab19f19fba45d94b2d7aa99665
blaqvikin <mawanda.mlalandle@outlook.com>
2022-07-11 21:33:31 +0200

    Added new file

--- Microsoft365/findAdminsInAzure.ps1
+++ Microsoft365/findAdminsInAzure.ps1
@@ -1,3 +1,3 @@
-$admin = Get-AzureADDirectoryRole | where{$_.displayname -like "global administrator"}
+$admin = Get-AzureADDirectoryRole | Where-Object {$_.displayname -like "Something Administrator"}
 
 Get-AzureADDirectoryRoleMember -objectId $admin.objectId
