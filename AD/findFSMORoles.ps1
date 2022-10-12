a9e67903958c60c83f6cb20d56c54d7fe8532773
blaqvikin <mawanda.mlalandle@outlook.com>
2022-10-12 09:26:27 +0200

    Updating local files

--- AD/findFSMORoles.ps1
+++ AD/findFSMORoles.ps1
@@ -1,3 +1,5 @@
 Get-ADDomainController -Filter * | Select-Object name, domain, forest, operationMasterRoles | Where-Object {$_.operationMasterRoles} | Format-Table -AutoSize
 
 #netdom.exe query fsmo
+
+Get-ADOptionalFeature -Identity "CN=Recycle Bin Feature,CN=Optional Features,CN=Directory Service,CN=Windows NT,CN=Services,CN=Configuration,DC=mifa,DC=local" -Scope ForestOrConfigurationSet –Target "mifa.local"
---
+++ Misc/SomeRandomPS.ps1
@@ -0,0 +1,11 @@
+get-help, get-command, get-module, get-member, get-psdrive, -whatif,
+
+get-aduser -filter & -resultpagesize $null |? lockedout
+
+search-adaccount -accountdisabled -useronly
+
+search-adaccount -lockedout -useronly | fl
+
+(search-adaccount -lockedout -useronly | fl).count
+
+get-service | ? status -eq "running" | select description
