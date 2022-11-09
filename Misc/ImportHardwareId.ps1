879c633bb5bd77ba41d2d00e3658f0542cd3b174
blaqvikin <mawanda.mlalandle@outlook.com>
2022-11-10 00:32:25 +0200

    Update

---
+++ Misc/ImportHardwareId.ps1
@@ -0,0 +1,11 @@
+$LaptopIPaddr = Import-Csv .\LaptopIps.csv
+
+foreach ($Laptop in $LaptopIPaddr) {
+    
+    New-Item -Type Directory -Path "C:\HWID"
+        Set-Location -Path "C:\HWID"
+    $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
+        Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+        Install-Script -Name Get-WindowsAutopilotInfo
+    Get-WindowsAutopilotInfo -OutputFile AutopilotHWID.csv
+}
