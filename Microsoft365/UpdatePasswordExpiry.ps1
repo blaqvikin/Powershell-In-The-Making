b3b7280c58c26f1e15cccd6acba2fc21e95b411d
blaqvikin <mawanda.mlalandle@outlook.com>
2023-07-18 05:58:23 +0200

    Updated password flag

--- Microsoft365/UpdatePasswordExpiry.ps1
+++ Microsoft365/UpdatePasswordExpiry.ps1
@@ -8,7 +8,6 @@ Description:
 Required Permission
 - User Access Administrator or any equivalent of in AAD
 #>
-
 # Install the required module
 Install-Module Microsoft.Graph
 
@@ -16,18 +15,21 @@ Install-Module Microsoft.Graph
 Connect-MgGraph -Scopes "User.Read.All"
 
 # Read users and export to CSV
-Get-MgUser -All | Select-Object UserPrincipalName, PasswordNeverExpires | Export-Csv -Path "accounts.csv" -NoTypeInformation
+Get-MgUser -All | Select-Object UserPrincipalName, PasswordNeverExpires | Export-Csv -Path "PathToCSVLocation" -NoTypeInformation
 
 # Import from CSV
-$Accounts = Import-Csv -Path "accounts.csv"
+$Accounts = Import-Csv -Path "PathToCSVLocation"
 
 # Loop through and update password never expires flag
 ForEach ($Account in $Accounts)
 {
     # Retrieve the user principal name and password never expires flag
     $UserPrincipalName = $Account.UserPrincipalName
-    $PasswordNeverExpires = $Account.PasswordNeverExpires
+    $PasswordNeverExpires = $False
 
     # Update the user's password never expires flag
-    Set-MgUser -UserPrincipalName $UserPrincipalName -PasswordProfile @{"PasswordNeverExpires" = $PasswordNeverExpires}
+    Set-MgUser -UserPrincipalName $UserPrincipalName -PasswordProfile @{"PasswordNeverExpires" = $PasswordNeverExpires} 
+
 }
+#Check the user
+<#Get-MGuser -All -Property UserPrincipalName, PasswordPolicies | Select-Object UserprincipalName,@{ N="PasswordNeverExpires";E={$_.PasswordPolicies -contains "DisablePasswordExpiration"}}#>
