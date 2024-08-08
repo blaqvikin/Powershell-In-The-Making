47d1236c2498077da115fcc4ba0714555dc6a4fa
blaqvikin <mawanda.mlalandle@outlook.com>
2024-08-08 13:36:29 +0200

    Sanitized the url

--- Intune/RemoveUsersFromLocalAdminGroup.ps1
+++ Intune/RemoveUsersFromLocalAdminGroup.ps1
@@ -4,7 +4,7 @@ If(!(test-path -PathType container $TempPath))
       New-Item -ItemType Directory -Path $TempPath
 
       #Download username csv file from a blob
-      wget "https://cloudshsan.blob.core.windows.net/publiccontainer/username.csv?sv=2020-10-02&si=publicpolicy&sr=b&sig=vSljYQXbgaWpTdkNqmLOAI6j1hVezS7xaAyRF5U5Q7g%3D" -OutFile $TempPath\username.csv
+      wget "URL" -OutFile $TempPath\username.csv
 
       #The users.csv is the file that contains all the users. The csv has a heading named "username"
       Import-Csv $TempPath\username.csv | ForEach-Object {Remove-LocalGroupMember -Group Administrators -Member $_.username}
